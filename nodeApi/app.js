d78d9fafc61a69608b70fa49359983d21d721766
Sarvar <sarvar.m149@gmail.com>
2021-07-19 03:19:00 +0700

    initial commit

---
+++ nodeApi/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const fs = require("fs");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = 3000;
+
+// db connection
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log("DB connection error: ${err.message}");
+});
+
+// bring in routes
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+// apiDocs
+app.get("/", (req, res) => {
+  fs.readFile("./docs/apiDocs.json", (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: err,
+      });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
+});
+
+// middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+app.use("/", postRoutes);
+app.use("/", authRoutes);
+app.use("/", userRoutes);
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: "Unauthorized!" });
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ nodeApi/controllers/user.js
@@ -0,0 +1,111 @@
+const _ = require("lodash");
+const User = require("../models/user");
+const formidable = require("formidable");
+const fs = require("fs");
+
+exports.userById = (req, res, next, id) => {
+  User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+    req.profile = user; // add profile object in req with user info
+    next();
+  });
+};
+
+exports.hasAuthorization = (req, res, next) => {
+  const authorized = req.profile && req.profile._id === req.auth._id;
+  if (!authorized) {
+    return res.status(403).json({
+      error: "User is not authorized to perform this action!",
+    });
+  }
+};
+
+exports.allUsers = (req, res) => {
+  User.find((err, users) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json(users);
+  }).select("name email updated created");
+};
+
+exports.getUser = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
+
+// exports.updateUser = (req, res, next) => {
+//   let user = req.profile;
+//   user = _.extend(user, req.body);
+//   user.updated = Date.now();
+//   user.save((err) => {
+//     if (err) {
+//       return res.status(400).json({
+//         error: "You are not authorized to perform this action",
+//       });
+//     }
+//     user.hashed_password = undefined;
+//     user.salt = undefined;
+//     res.json({ user });
+//   });
+// };
+
+exports.updateUser = (req, res, next) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Photo could not be uploaded",
+      });
+    }
+    // save user
+    let user = req.profile;
+    user = _.extend(user, fields);
+    user.updated = Date.now();
+
+    if (files.photo) {
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contentType = files.photo.type;
+    }
+
+    user.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json(user);
+    });
+  });
+};
+
+exports.userPhoto = (req, res, next) => {
+  if (req.profile.photo.data) {
+    res.set("Content-Type", req.profile.photo.contentType);
+    return res.send(req.profile.photo.data);
+  }
+  next();
+};
+
+exports.deleteUser = (req, res, next) => {
+  let user = req.profile;
+  user.remove((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: "You are not authorized to perform this action",
+      });
+    }
+
+    res.json({ message: "User deleted successfully" });
+  });
+};
